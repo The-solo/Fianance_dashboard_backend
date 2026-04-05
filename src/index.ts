b893220ee2cb3ebe31398df90f018e1174da8bfc
The-solo <utkarshsase4321@gmail.com>
2026-04-05 12:45:07 +0530

    added basic server logging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import routes from "./routes";
+import morgan from "morgan"
 
 const app = express();
+app.use(morgan("dev"))  // Adding the basic sever logging.
 app.use(express.json());
 app.use("/api", routes);
 
